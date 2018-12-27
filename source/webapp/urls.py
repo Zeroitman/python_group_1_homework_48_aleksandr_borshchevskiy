19737a2cd2f9aef38d5a3b4b6ee28e5ee58e959f
Zeroitman <borsh130894@gmail.com>
2018-12-27 16:11:30 +0600

    Update_9

---
+++ source/webapp/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from webapp.views import OrderListView, OrderCreateView, OrderfoodCreateView, OrderUpdateView,\
+                            FoodCreateView, FoodDeleteView, FoodUpdateView, FoodListView, \
+                            OrderfoodDeleteView, CourierListView, change_status, change_status_courier_1,\
+                            change_status_courier_2, DetailListView
+app_name = 'webapp'
+
+urlpatterns = [
+    path('', OrderListView.as_view(), name='order_list'),
+    path('menu', FoodListView.as_view(), name='food_list'),
+    path('food/create', FoodCreateView.as_view(), name='food_add'),
+    path('food/<int:pk>/update', FoodUpdateView.as_view(), name='food_update'),
+    path('food/<int:pk>/delete', FoodDeleteView.as_view(), name='food_delete'),
+    path('order_food/<int:pk>/delete_food', OrderfoodDeleteView.as_view(), name='order_food_delete'),
+    path('order/create', OrderCreateView.as_view(), name='order_add'),
+    path('order/<int:pk>/order_food', OrderfoodCreateView.as_view(), name='order_food_add'),
+    path('courier', CourierListView.as_view(), name='courier_list'),
+    path('order/<int:pk>/update', OrderUpdateView.as_view(), name='order_update'),
+    path('order/<int:pk>/change_status', change_status, name='change_status'),
+    path('order/<int:pk>/change_status_courier_1', change_status_courier_1, name='change_status_courier_1'),
+    path('order/<int:pk>/change_status_courier_2', change_status_courier_2, name='change_status_courier_2'),
+    path('order_food/<int:pk>/update', DetailListView.as_view(), name='order_detail'),
+]
+
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,8 +1,106 @@
 from django.shortcuts import reverse, redirect, get_object_or_404
-from webapp.models import Food, Order, OrderFood, Employee
-from django.views.generic import ListView, CreateView, DeleteView, UpdateView
+from webapp.models import Food, Order, OrderFood
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView, DetailView
 from webapp.forms import ProjectForm, OrderForm, OrderFoodForm
 from django.urls import reverse_lazy
+
+
+class OrderListView(ListView):
+    model = Order
+    template_name = 'order_list.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('%s' % reverse('webauth:login'))
+        return super().dispatch(request, *args, **kwargs)
+
+
+class OrderCreateView(CreateView):
+    model = Order
+    template_name = 'order_add.html'
+    form_class = OrderForm
+
+    def get_success_url(self):
+        return reverse('webapp:order_food_add', kwargs={'pk': self.object.pk})
+
+
+class OrderUpdateView(UpdateView):
+    model = Order
+    template_name = 'order_update.html'
+    form_class = OrderForm
+    success_url = reverse_lazy('webapp:order_list')
+
+
+class CourierListView(ListView):
+    model = Order
+    template_name = 'courier_list.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('%s' % reverse('webauth:login'))
+        return super().dispatch(request, *args, **kwargs)
+
+
+class FoodListView(ListView):
+    model = Food
+    template_name = 'food_list.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            return redirect('%s' % reverse('webauth:login'))
+        return super().dispatch(request, *args, **kwargs)
+
+
+class FoodCreateView(CreateView):
+    model = Food
+    template_name = 'food_add.html'
+    form_class = ProjectForm
+    success_url = reverse_lazy('webapp:food_list')
+
+
+class FoodDeleteView(DeleteView):
+    model = Food
+    template_name = 'food_delete.html'
+    form_class = ProjectForm
+    success_url = reverse_lazy('webapp:food_list')
+
+
+class FoodUpdateView(UpdateView):
+    model = Food
+    template_name = 'food_update.html'
+    form_class = ProjectForm
+    success_url = reverse_lazy('webapp:food_list')
+
+
+class DetailListView(DetailView):
+    model = Order
+    template_name = 'order_detail.html'
+
+
+class OrderfoodCreateView(CreateView):
+    model = OrderFood
+    template_name = 'order_food_add.html'
+    form_class = OrderFoodForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['order_pk'] = self.kwargs.get('pk')
+        return context
+
+    def form_valid(self, form):
+        form.instance.order = Order.objects.get(pk=self.kwargs.get('pk'))
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('webapp:order_food_add', kwargs={'pk': self.kwargs.get('pk')})
+
+
+class OrderfoodDeleteView(DeleteView):
+    model = OrderFood
+    template_name = 'order_food_delete.html'
+
+    def get_success_url(self):
+        return reverse('webapp:order_detail', kwargs={'pk': get_object_or_404(OrderFood, pk=self.kwargs.get('pk')).order.pk})
 
 
 def change_status(request, pk):
@@ -25,81 +123,3 @@ def change_status_courier_2(request, pk):
     task.status = 'Доставлен'
     task.save()
     return redirect('webapp:courier_list')
-
-
-class ClientListView(ListView):
-    model = Employee
-    template_name = 'client_list.html'
-
-
-class OrderListView(ListView):
-    model = Order
-    template_name = 'order_list.html'
-
-
-class CourierListView(ListView):
-    model = Order
-    template_name = 'courier_list.html'
-
-
-class OrderCreateView(CreateView):
-    model = Order
-    template_name = 'order_add.html'
-    form_class = OrderForm
-
-    def get_success_url(self):
-        return reverse('webapp:order_food_add', kwargs={'pk': self.object.pk})
-
-
-class FoodListView(ListView):
-    model = Food
-    template_name = 'food_list.html'
-
-
-class FoodCreateView(CreateView):
-    model = Food
-    template_name = 'food_add.html'
-    form_class = ProjectForm
-    success_url = reverse_lazy('webapp:order_list')
-
-
-class FoodDeleteView(DeleteView):
-    model = Food
-    template_name = 'food_delete.html'
-    form_class = ProjectForm
-    success_url = reverse_lazy('webapp:food_list')
-
-
-class FoodUpdateView(UpdateView):
-    model = Food
-    template_name = 'food_update.html'
-    form_class = ProjectForm
-    success_url = reverse_lazy('webapp:food_list')
-
-
-class OrderfoodCreateView(CreateView):
-    model = OrderFood
-    template_name = 'order_food_add.html'
-    form_class = OrderFoodForm
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['order_pk'] = self.kwargs.get('pk')
-        return context
-
-    def form_valid(self, form):
-        form.instance.order = Order.objects.get(pk=self.kwargs.get('pk'))
-        return super().form_valid(form)
-
-    def get_success_url(self):
-        return reverse('webapp:order_food_add', kwargs={'pk': self.kwargs.get('pk')})
-
-
-class OrderUpdateView(UpdateView):
-    model = Order
-    template_name = 'order_update.html'
-    form_class = OrderForm
-    success_url = reverse_lazy('webapp:order_list')
-
-
-
